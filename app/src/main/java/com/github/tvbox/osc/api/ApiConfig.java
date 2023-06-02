c675cacd78d9b873fd4aca95b137c8033bd8f269
lli8779182 <108140488+lli8779182@users.noreply.github.com>
2023-06-02 12:46:17 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -127,7 +127,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://jihulab.com/ss2/ss/-/raw/main/dd.bmp");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
